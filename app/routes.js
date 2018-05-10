768c919e0cecd6cf0062162dfbc9147b298112e6
Simone Duca <simone.duca@gmail.com>
2018-05-10 17:01:02 +0100

    Add macros. Change view engine to njk. Add customers route.

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,10 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/customers', function (req, res) {
+  res.render('customers')
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
