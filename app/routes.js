8823826840a68f647f46b1faba0839a40fd28711
Simone Duca <simone.duca@gmail.com>
2018-05-11 16:46:47 +0100

    Add a customers list view. Organise folders.

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+// Mock data
+const data = require('./mock-data.json')
+
 // Route index page
 router.get('/', function (req, res) {
   res.render('index')
 })
 
 router.get('/customers', function (req, res) {
+  res.locals.customers = data
   res.render('customers')
 })
 
