bf8f7e55dfc766e9b4dd695582da74a395eff480
Simone Duca <simone.duca@gmail.com>
2018-05-14 15:13:01 +0100

    Add search bar and actions dropdown.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,40 @@ $(document).ready(function () {
   // to toggle hidden content
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
+
+  // Table search
+  if(document.getElementById('searchReducer')) {
+    document.getElementById('searchReducer').addEventListener('change', findMatchingRows);
+    document.getElementById('searchReducer').addEventListener('keyup', findMatchingRows);
+    document.querySelectorAll('.table-click-helper').forEach(function(section) {
+        section.addEventListener('click', clickTableCheckbox);
+    });
+  }
+
+  function findMatchingRows() {
+    var regex = new RegExp(this.value, 'gi');
+    document.querySelectorAll('#searchableTable tr').forEach(function(tableRow) {
+        tableRow.classList.add('js-hidden');
+        if(tableRow.dataset.search.match(regex)) {
+            tableRow.classList.remove('js-hidden');
+        }
+    });
+  }
+
+  function findSelectedMatches() {
+      var self = this;
+      document.querySelectorAll('#searchableTable tr').forEach(function(tableRow) {
+          tableRow.classList.add('js-hidden');
+          if(self.value === 'noFilter') {
+              tableRow.classList.remove('js-hidden');
+          } else if(tableRow.dataset.role.match(self.value)) {
+              tableRow.classList.remove('js-hidden');
+          }
+      });
+  }
+
+  function clickTableCheckbox() {
+      document.getElementById(this.dataset.checkbox).click();
+  }
+
 })
